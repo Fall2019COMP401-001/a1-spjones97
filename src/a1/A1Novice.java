f86a460aa071cf5e5f54c6ad1ec776ebd012d695
spjones97 <43318183+spjones97@users.noreply.github.com>
2019-08-29 21:22:23 -0400

    Update A1Novice.java

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,22 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-		
+		int customers = scan.nextInt();
+		for (int i = 0; i < customers; i++) {
+			String firstName = scan.next();
+			char firstInitial = firstName.charAt(0);
+			String lastName = scan.next();
+			int itemNumber = scan.nextInt();
+			double total = 0;
+			for (int j = 0; j < itemNumber; j++) {
+				int amount = scan.nextInt();
+				String itemName = scan.next();
+				double cost = scan.nextDouble();
+				total += amount * cost;
+			}
+			System.out.println(firstInitial + ". " + lastName + ": " + total);
+		}
+
+		scan.close();
 	}
 }
